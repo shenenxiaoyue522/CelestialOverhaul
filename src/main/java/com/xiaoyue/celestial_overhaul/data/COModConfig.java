0a390316317e0bb96427c7ecbaf70132199b0ac5
shenenxiaoyue <3371025687@qq.com>
2026-03-06 23:19:17 +0800

    aabb

--- src/main/java/com/xiaoyue/celestial_overhaul/data/COModConfig.java
+++ src/main/java/com/xiaoyue/celestial_overhaul/data/COModConfig.java
@@ -14,7 +14,8 @@ public class COModConfig {
 		public Client(Builder builder) {
 			builder.push("function", "Function");
 			hideEffectiveFireScreen = builder
-					.name("Hide the flame screen when it has the flame resistance effect")
+					.name("Hide Effective Fire Screen")
+					.comment("Hide the flame screen when it has the flame resistance effect")
 					.define("hideEffectiveFireScreen", true);
 			builder.pop();
 		}
@@ -29,9 +30,6 @@ public class COModConfig {
 		public final ModConfigSpec.DoubleValue playerHungryDamageTweak;
 		public final ModConfigSpec.DoubleValue entityCrammingDamageTweak;
 		public final ModConfigSpec.DoubleValue entityDrownDamageTweak;
-		public final ModConfigSpec.DoubleValue sharpnessEnchantmentBonus;
-		public final ModConfigSpec.DoubleValue killerEnchantmentBonus;
-		public final ModConfigSpec.DoubleValue thornsEnchantmentTweak;
 		public final ModConfigSpec.DoubleValue strengthEffectDamageBonus;
 		public final ModConfigSpec.DoubleValue weaknessEffectDamageReduce;
 		public final ModConfigSpec.DoubleValue resistanceEffectTweak;
@@ -49,43 +47,41 @@ public class COModConfig {
 		public Server(Builder builder) {
 			builder.push("misc", "Misc");
 			grassPiercingAttack = builder
-					.name("Ignore the nearby grass when attacking")
+					.name("Grass Piercing Attack")
+					.comment("Ignore the nearby grass when attacking")
 					.define("grassPiercingAttack", true);
 			canUseSwordBlock = builder
-					.name("Enable sword holding block")
+					.name("Enable Sword Holding Block")
 					.define("canUseSwordBlock", true);
 			arrowHitMobSound = builder
-					.name("When an arrow hits a non-player mob, it also makes a hit sound")
+					.name("Arrow Hit Sound")
+					.comment("When an arrow hits a non-player mob, it also makes a hit sound")
 					.define("arrowHitMobSound", true);
 			canVanillaCritical = builder
-					.name("Whether to allow vanilla crit")
+					.name("Allow Vanilla Crit")
 					.define("canVanillaCritical", true);
 			builder.pop();
 
 			builder.push("enchantments_and_effects", "Enchantments And Effects");
-			sharpnessEnchantmentBonus = builder
-					.name("Sharp enchantment damage bonus as percentage bonus")
-					.comment("A value of -1 disables this function")
-					.defineInRange("sharpnessEnchantmentBonus", 0.06, -1, 10);
-			killerEnchantmentBonus = builder
-					.name("Smite-like enchantment damage bonus as percentage bonus")
-					.comment("A value of -1 disables this function")
-					.defineInRange("killerEnchantmentTweak", 0.12, -1, 10);
 			strengthEffectDamageBonus = builder
-					.name("Strength effect damage bonus per level")
+					.name("Strength Effect Tweak")
+					.comment("Strength effect damage bonus per level")
 					.comment("A value of -1 disables this function")
 					.defineInRange("strengthEffectDamageBonus", 0.3, -1, 10);
 			weaknessEffectDamageReduce = builder
-					.name("Weakness effect damage reduce per level")
+					.name("Weakness Effect Tweak")
+					.comment("Weakness effect damage reduce per level")
 					.comment("A value of -1 disables this function")
 					.defineInRange("weaknessEffectDamageReduce", -1, -1, 10.0);
 			resistanceEffectTweak = builder
-					.name("Multiplicative stacking factor for Resistance Effect")
+					.name("Resistance Effect Tweak")
+					.comment("Multiplicative stacking factor for Resistance Effect")
 					.comment("newDamage = incomingDamage * factor ^ level")
 					.comment("A value of -1 disables this function")
 					.defineInRange("resistanceEffectTweak", 0.8, -1, 10);
 			protectionEnchantmentTweak = builder
-					.name("Multiplicative stacking factor for Protection-like Enchantments")
+					.name("Protection Enchantment Tweak")
+					.comment("Multiplicative stacking factor for Protection-like Enchantments")
 					.comment("newDamage = incomingDamage * factor ^ totalPoints")
 					.comment("A value of -1 disables this function")
 					.defineInRange("protectionEnchantmentTweak", 0.96, -1, 1);
@@ -93,66 +89,73 @@ public class COModConfig {
 
 			builder.push("max_health_based_scaling", "Max Health Based Scaling");
 			scalingToPlayerOnly = builder
-					.name("All damage scaling applies to player only")
+					.name("Scaling To Player Only")
+					.comment("All damage scaling applies to player only")
 					.define("scalingToPlayerOnly",false);
 			fireTypeDamageTweak = builder
-					.name("Fire type damage scales with max health")
+					.name("Fire Type Damage Tweak")
+					.comment("Fire type damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("fireTypeDamageTweak", 0.02, -1, 1);
 			frozenTypeDamageTweak = builder
-					.name("Frozen type damage scales with max health")
+					.name("Frozen Type Damage Tweak")
+					.comment("Frozen type damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("frozenTypeDamageTweak", 0.02, -1, 1);
 			playerHungryDamageTweak = builder
-					.name("Starvation damage scales with max health")
+					.name("Player Hungry Damage Tweak")
+					.comment("Starvation damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("playerHungryDamageTweak", 0.02, -1, 1);
 			entityCrammingDamageTweak = builder
-					.name("Suffocation and Cramming damage scales with max health")
+					.name("Entity Cramming Damage Tweak")
+					.comment("Suffocation and Cramming damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("entityCrammingDamageTweak", 0.02, -1, 1);
 			entityDrownDamageTweak = builder
-					.name("Drowning damage scales with max health")
+					.name("Entity Drown Damage Tweak")
+					.comment("Drowning damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("entityDrownDamageTweak", 0.02, -1, 1);
 			entityFallDamageTweak = builder
-					.name("Fall damage scales with max health")
+					.name("Entity Fall Damage Tweak")
+					.comment("Fall damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("entityFallDamageTweak", -1, -1, 1f);
 			poisonEffectDamageTweak = builder
-					.name("Poison damage scales with current health")
+					.name("Poison Effect Tweak")
+					.comment("Poison damage scales with current health")
 					.comment("newDamage = oldDamage * max(1, factor * health)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("poisonEffectDamageTweak", 0.05, -1, 1);
 			witherEffectDamageTweak = builder
-					.name("Wither effect damage scales with max health")
+					.name("Wither Effect Tweak")
+					.comment("Wither effect damage scales with max health")
 					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("witherEffectDamageTweak", 0.02, -1, 1);
-			thornsEnchantmentTweak = builder
-					.name("Thorn damage scales with max health")
-					.comment("newDamage = oldDamage * max(1, factor * maxHealth)")
-					.comment("A value of -1 disables this function")
-					.defineInRange("thornsEnchantmentTweak", 0.02, -1, 1);
 
 			regenerationEffectTweak = builder
-					.name("Regeneration effect scales with max health")
+					.name("Regeneration Effect Tweak")
+					.comment("Regeneration effect scales with max health")
 					.comment("newHealing = oldHealing * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("regenerationEffectTweak", 0.05, -1, 1);
 			fullFoodLevelHealTweak = builder
-					.name("Natural healing scales with max health")
+					.name("Full Food Level Heal Tweak")
+					.comment("Natural healing scales with max health")
 					.comment("newHealing = oldHealing * max(1, factor * maxHealth)")
 					.comment("A value of -1 disables this function")
 					.defineInRange("fullFoodLevelHealTweak", 0.05, -1, 1);
 			maxHealAmountTweak = builder
-					.name("Entities are bound by this value when they healed")
+					.name("Max Heal Amount Tweak")
+					.comment("Entities are bound by this value when they healed")
 					.comment("newHealing = maxHealth * factor")
 					.comment("A value of -1 disables this function")
 					.defineInRange("maxHealAmountTweak", -1, -1, Double.MAX_VALUE);
--- src/main/java/com/xiaoyue/celestial_overhaul/event/COGeneralEventHandler.java
+++ src/main/java/com/xiaoyue/celestial_overhaul/event/COGeneralEventHandler.java
@@ -1,6 +1,5 @@
 package com.xiaoyue.celestial_overhaul.event;
 
-import com.xiaoyue.celestial_invoker.content.common.Bindings;
 import com.xiaoyue.celestial_overhaul.content.EffectOverrideHandler;
 import com.xiaoyue.celestial_overhaul.content.OverhaulUtils;
 import com.xiaoyue.celestial_overhaul.content.WeaponBlockHandler;
@@ -8,7 +7,6 @@ import com.xiaoyue.celestial_overhaul.data.COModConfig;
 import com.xiaoyue.celestial_overhaul.data.COTagGen;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.DamageTypeTags;
-import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EntitySelector;
@@ -17,7 +15,6 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.neoforged.bus.api.EventPriority;
@@ -110,32 +107,6 @@ public class COGeneralEventHandler {
 			Double config = COModConfig.SERVER.fireTypeDamageTweak.get();
 			if (config >= 0) {
 				event.setNewDamage(event.getOriginalDamage() * Math.max(1, entity.getMaxHealth() * config.floatValue()));
-			}
-		}
-	}
-
-	@SubscribeEvent
-	public static void onHurtLiving(LivingIncomingDamageEvent event) {
-		DamageSource source = event.getSource();
-		LivingEntity entity = event.getEntity();
-		if (!(source.getEntity() instanceof LivingEntity attacker)) return;
-		if (source.getDirectEntity() != null && source.getDirectEntity().equals(attacker)) {
-			ItemStack stack = attacker.getMainHandItem();
-			Double sharpnessConfig = COModConfig.SERVER.sharpnessEnchantmentBonus.get();
-			int sharpnessLv = Bindings.getEnchantmentLv(stack, Enchantments.SHARPNESS);
-			if (sharpnessLv > 0 && sharpnessConfig >= 0) {
-				event.setAmount(event.getAmount() * (1 + sharpnessConfig.floatValue() * sharpnessLv));
-			}
-			Double killerConfig = COModConfig.SERVER.killerEnchantmentBonus.get();
-			if (killerConfig >= 0) {
-				int smiteLv = Bindings.getEnchantmentLv(stack, Enchantments.SMITE);
-				if (smiteLv > 0 && entity.getType().is(EntityTypeTags.UNDEAD)) {
-					event.setAmount(event.getAmount() * (1 + killerConfig.floatValue() * smiteLv));
-				}
-				int boaLv = Bindings.getEnchantmentLv(stack, Enchantments.BANE_OF_ARTHROPODS);
-				if (boaLv > 0 && entity.getType().is(EntityTypeTags.ARTHROPOD)) {
-					event.setAmount(event.getAmount() * (1 + killerConfig.floatValue() * boaLv));
-				}
 			}
 		}
 	}
