31bbb6855067b22812ade1f8b99c12f1ce9ecd02
shenenxiaoyue <3371025687@qq.com>
2026-09-07 17:58:32 +0800

    1.1.7

--- src/main/java/com/xiaoyue/celestial_overhaul/mixin/ItemInHandRenderMixin.java
+++ src/main/java/com/xiaoyue/celestial_overhaul/mixin/ItemInHandRenderMixin.java
@@ -2,16 +2,13 @@ package com.xiaoyue.celestial_overhaul.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.xiaoyue.celestial_overhaul.content.WeaponBlockHandler;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,22 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemInHandRenderer.class)
 public abstract class ItemInHandRenderMixin {
 
-	@Shadow
-	protected abstract void applyItemArmTransform(PoseStack pPoseStack, HumanoidArm pHand, float pEquippedProg);
-
-	@Inject(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;getUseAnimation()Lnet/minecraft/world/item/UseAnim;"), method = "renderArmWithItem", cancellable = true)
+	@Inject(method = "renderArmWithItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;renderItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;ZLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"))
 	public void celestial_overhaul$renderSwordBlockInHand(AbstractClientPlayer pPlayer, float pPartialTicks, float pPitch, InteractionHand pHand, float pSwingProgress, ItemStack pStack, float pEquippedProgress, PoseStack pPoseStack, MultiBufferSource pBuffer, int pCombinedLight, CallbackInfo ci) {
-		if (WeaponBlockHandler.canBlock(pStack)) {
-			pPoseStack.pushPose();
-			boolean flag = pHand.equals(InteractionHand.MAIN_HAND);
+		if (pPlayer.isUsingItem() && pPlayer.getUsedItemHand() == pHand && WeaponBlockHandler.canBlock(pStack)) {
+			boolean flag = pHand == InteractionHand.MAIN_HAND;
 			HumanoidArm arm = flag ? pPlayer.getMainArm() : pPlayer.getMainArm().getOpposite();
-			boolean right = arm.equals(HumanoidArm.RIGHT);
-			this.applyItemArmTransform(pPoseStack, arm, pEquippedProgress);
-			WeaponBlockHandler.renderBlock(pPoseStack, right);
-			ItemInHandRenderer renderer = Minecraft.getInstance().getEntityRenderDispatcher().getItemInHandRenderer();
-			renderer.renderItem(pPlayer, pStack, right ? ItemDisplayContext.FIRST_PERSON_RIGHT_HAND : ItemDisplayContext.FIRST_PERSON_LEFT_HAND, !right, pPoseStack, pBuffer, pCombinedLight);
-			pPoseStack.popPose();
-			ci.cancel();
+			WeaponBlockHandler.renderBlock(pPoseStack, arm == HumanoidArm.RIGHT);
 		}
 	}
 }
